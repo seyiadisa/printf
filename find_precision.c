0c6f894d823c3c2a17612c432d14466129674f5e
Pop-Ace <gbengapopoola01@yahoo.com>
2023-03-31 10:26:33 -0700

    Add:precision

--- find_precision.c
+++ find_precision.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * find_precision - Calculates precisions for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int find_precision(const char *format, int *i, va_list list)
 {
 	int curr = *i + 1;
 	int precision = -1;
